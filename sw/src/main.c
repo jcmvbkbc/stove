2867592839c6a7177fe9e1054992a3c19db1cf3e
Max Filippov <jcmvbkbc@gmail.com>
2013-12-07 15:21:35 +0400

    sw: switch to delay_basic, fix thermometer input
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- sw/src/main.c
+++ sw/src/main.c
@@ -1,6 +1,8 @@
+#define F_CPU 1000000
+
 #include <inttypes.h>
 #include <avr/io.h>
-#include <util/delay.h>
+#include <util/delay_basic.h>
 
 #define TERM_DDR  DDRB
 #define TERM_PORT PORTB
@@ -8,13 +10,12 @@
 
 #define HEAT_DDR  DDRC
 #define HEAT_PORT PORTC
-#define HEAT_BIT  PB5
+#define HEAT_BIT  PC5
 
 int main() {
 	TERM_DDR |= _BV(TERM_BIT);
 	HEAT_DDR |= _BV(HEAT_BIT);
 
 	while(1) {
-		;
 	}
 }
