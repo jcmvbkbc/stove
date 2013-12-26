3a79a3cfcb2c0402ad9a6451d1528346d1a6e2be
Max Filippov <jcmvbkbc@gmail.com>
2013-12-26 06:57:42 +0400

    sw: fix eeprom write, add eeprom read
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- sw/src/main.c
+++ sw/src/main.c
@@ -29,13 +29,25 @@ void set_pending_irq(uint8_t irq)
 	pending_irq |= irq;
 }
 
-void eeprom_write(unsigned addr, unsigned char data)
+void eeprom_write(unsigned addr, uint8_t data)
 {
+	uint8_t flags;
+
 	while (EECR & _BV(EEPE));
 	EEAR = addr;
 	EEDR = data;
+	flags = cli_save_irq();
 	EECR = _BV(EEMPE);
 	EECR |= _BV(EEPE);
+	restore_irq(flags);
+}
+
+uint8_t eeprom_read(unsigned addr)
+{
+	while (EECR & _BV(EEPE));
+	EEAR = addr;
+	EECR |= _BV(EERE);
+	return EEDR;
 }
 
 static void uart_init(void)
