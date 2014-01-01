48b09b6239ed3815358c6d97c51eb9c36023f662
Max Filippov <jcmvbkbc@gmail.com>
2014-01-01 07:24:18 +0400

    sw: add core state save/load
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ sw/src/state.c
@@ -0,0 +1,71 @@
+#include <stddef.h>
+#include "eeprom.h"
+#include "state.h"
+
+#define CRC_POLY 0xedb88320
+#define EEPROM_STOVE_STATE 16
+#define EEPROM_STOVE_PROG  64
+
+static uint32_t crc(const void *p, uint8_t sz)
+{
+	uint8_t i, j;
+	uint32_t crc = ~0;
+
+	for (i = 0; i < sz; ++i) {
+		crc ^= ((uint8_t *)p)[i];
+		for (j = 0; j < 8; ++j)
+			if (crc & 1)
+				crc = (crc >> 1) ^ CRC_POLY;
+			else
+				crc >>= 1;
+	}
+	return ~crc;
+}
+
+static void generic_save(const void *p, uint8_t sz, uint16_t base)
+{
+	uint8_t i;
+
+	for (i = 0; i < sz; ++i) {
+		uint8_t v = ((uint8_t *)p)[i];
+
+		if (eeprom_read(base + i) != v)
+			eeprom_write(base + i, v);
+	}
+}
+
+static void generic_load(void *p, uint8_t sz, uint16_t base)
+{
+	uint8_t i;
+
+	for (i = 0; i < sz; ++i)
+		((uint8_t *)p)[i] = eeprom_read(base + i);
+}
+
+uint8_t state_load(struct stove_state *state)
+{
+	generic_load(state, sizeof(*state), EEPROM_STOVE_STATE);
+	return crc(state, sizeof(*state)) == 0;
+}
+
+uint8_t state_save(struct stove_state *state)
+{
+	state->crc = crc(state, offsetof(struct stove_state, crc));
+	generic_save(state, sizeof(*state), EEPROM_STOVE_STATE);
+	return state_load(state);
+}
+
+uint8_t state_load_prog(struct stove_prog *prog, uint8_t idx)
+{
+	generic_load(prog, sizeof(*prog),
+		     EEPROM_STOVE_PROG + sizeof(*prog) * idx);
+	return crc(prog, sizeof(*prog)) == 0;
+}
+
+uint8_t state_save_prog(struct stove_prog *prog, uint8_t idx)
+{
+	prog->crc = crc(prog, offsetof(struct stove_prog, crc));
+	generic_save(prog, sizeof(*prog),
+		     EEPROM_STOVE_PROG + sizeof(*prog) * idx);
+	return state_load_prog(prog, idx);
+}
---
+++ sw/src/state.h
@@ -0,0 +1,33 @@
+#ifndef _STATE_H
+#define _STATE_H
+
+#include <inttypes.h>
+
+#define MODE_THERMOSTAT 0xff
+
+struct stove_point {
+	uint16_t time; /* point time (minutes) */
+	int16_t t; /* point temperature */
+};
+
+struct stove_prog {
+	struct stove_point point[15];
+	uint32_t crc;
+};
+
+struct stove_state {
+	uint32_t cur_time; /* session runtime */
+	uint32_t thermostat_time; /* total session time for thermostat mode, 0 == no limit */
+	int16_t thermostat_t; /* thermostat target temperature */
+	uint8_t mode; /* program index, or MODE_THERMOSTAT */
+	uint8_t n_prog; /* number of predefined programs */
+	uint32_t crc;
+};
+
+uint8_t state_load(struct stove_state *state);
+uint8_t state_save(struct stove_state *state);
+
+uint8_t state_load_prog(struct stove_prog *prog, uint8_t idx);
+uint8_t state_save_prog(struct stove_prog *prog, uint8_t idx);
+
+#endif /* _STATE_H */
