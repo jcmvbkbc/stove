72d0b7c78499d25c1165401b6771cf538cefb809
Max Filippov <jcmvbkbc@gmail.com>
2014-01-03 09:42:48 +0400

    sw: add generic purpose timers
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- sw/src/timer.c
+++ sw/src/timer.c
@@ -1,9 +1,19 @@
+#include <stddef.h>
 #include "interrupt.h"
 #include "timer.h"
 
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
 #define TIMER_TOP ((F_CPU) / 8000L) //for 1 ms with prescaler 8
 
+struct timer {
+	uint32_t time;
+	void (*f)(void *p);
+	void *p;
+};
+
 static volatile uint32_t ms_timer;
+static uint32_t timer_min = 0xffffffff;
+static struct timer timer[20];
 
 void timer_init(void)
 {
@@ -17,7 +27,8 @@ void timer_init(void)
 
 ISR(TIMER0_COMPA_vect)
 {
-	ms_timer++;
+	if (++ms_timer >= timer_min)
+		set_pending_irq(IRQ_TIMER);
 }
 
 uint32_t timer_get_time(void)
@@ -30,4 +41,51 @@ uint32_t timer_get_time(void)
 	restore_irq(flags);
 
 	return res;
+}
+
+void timer_process_timers(void)
+{
+	uint32_t now;
+
+	while ((now = timer_get_time()) >= timer_min) {
+		uint32_t min = 0xffffffff;
+		uint8_t i;
+
+		for (i = 0; i < ARRAY_SIZE(timer); ++i) {
+			if (timer[i].f && timer[i].time <= now) {
+				void (*f)(void *) = timer[i].f;
+
+				timer[i].f = NULL;
+				f(timer[i].p);
+			}
+		}
+		for (i = 0; i < ARRAY_SIZE(timer); ++i) {
+			if (timer[i].f && timer[i].time < min)
+				min = timer[i].time;
+		}
+		timer_min = min;
+	}
+}
+
+uint8_t timer_add(uint32_t timeout, void (*f)(void *p), void *p)
+{
+	uint8_t i;
+
+	for (i = 0; i < ARRAY_SIZE(timer); ++i) {
+		if (!timer[i].f) {
+			timer[i].f = f;
+			timer[i].p = p;
+			timer[i].time = timer_get_time() + timeout;
+			if (timer[i].time < timer_min)
+				timer_min = timer[i].time;
+			return i;
+		}
+	}
+	return TIMER_INVALID;
+}
+
+void timer_del(uint8_t id)
+{
+	if (id < ARRAY_SIZE(timer))
+		timer[id].f = NULL;
 }
--- sw/src/timer.h
+++ sw/src/timer.h
@@ -3,7 +3,13 @@
 
 #include <inttypes.h>
 
+#define TIMER_INVALID 0xff
+
 void timer_init(void);
 uint32_t timer_get_time(void);
+
+void timer_process_timers(void);
+uint8_t timer_add(uint32_t timeout, void (*f)(void *p), void *p);
+void timer_del(uint8_t id);
 
 #endif
